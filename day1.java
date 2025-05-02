29f1402e1d0b15b2c4af174306575bfc243a546b
Harshil <harshilmayani108@gmail.com>
2025-05-02 12:21:21 +0530

    day 1

--- day1.java
+++ day1.java
@@ -2,30 +2,55 @@ import java.util.Scanner;
 public class day1 {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
-        //basic input output
+        // //basic input output
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter your name:");
-        String name = sc.nextLine();
-        System.out.println("Hello, " + name + "!");
-        //basic arithmetic operations
-        System.out.println("Enter two numbers:");
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-        System.out.println("Sum: " + (a + b));
-        System.out.println("Difference: " + (a - b));
-        System.out.println("Product: " + (a * b));
-        System.out.println("Quotient: " + (a / b));
-        System.out.println("Remainder: " + (a % b));
-        //decision making statements
-        System.out.println("Enter a number:");
-        int num = sc.nextInt();
-        if (num > 0) {
-            System.out.println(num + " is positive.");
-        } else if (num < 0) {
-            System.out.println(num + " is negative.");
+        // System.out.println("Enter your name:");
+        // String name = sc.nextLine();
+        // System.out.println("Hello, " + name + "!");
+        // //basic arithmetic operations
+        // System.out.println("Enter two numbers:");
+        // int a = sc.nextInt();
+        // int b = sc.nextInt();
+        // System.out.println("Sum: " + (a + b));
+        // System.out.println("Difference: " + (a - b));
+        // System.out.println("Product: " + (a * b));
+        // System.out.println("Quotient: " + (a / b));
+        // System.out.println("Remainder: " + (a % b));
+        // //decision making statements
+        // System.out.println("Enter a number:");
+        // int num = sc.nextInt();
+        // if (num > 0) {
+        //     System.out.println(num + " is positive.");
+        // } else if (num < 0) {
+        //     System.out.println(num + " is negative.");
+        // } else {
+        //     System.out.println("The number is zero.");
+        // }
+//odd or even
+        // System.out.println("Enter a number:");
+        // int number = sc.nextInt();
+        // if (number % 2 == 0) {
+        //     System.out.println(number + " is even.");
+        // } else {
+        //     System.out.println(number + " is odd.");
+        // }
+        // int division = sc.nextInt();
+        // if (division% 3 == 0 && division%5 == 0) {
+        //     System.out.println(division + " is divisible by both 3 and 5.");
+        // } else if (division%3 == 0) {
+        //     System.out.println(division + " is divisible by 3.");
+        // } else if (division%5 == 0) {
+        //     System.out.println(division + " is divisible by 5.");
+        // } else {
+        //     System.out.println(division + " is not divisible by either 3 or 5.");
+        // }
+        //vowels and consonants
+        char ch = sc.next().charAt(0);
+        if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' ||
+            ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U') {
+            System.out.println(ch + " is a vowel.");
         } else {
-            System.out.println("The number is zero.");
+            System.out.println(ch + " is a consonant.");
         }
-
     }
 }
