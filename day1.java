539c7eb97f93d9f358b16a69b00cea08c59e4dbe
Harshil <harshilmayani108@gmail.com>
2025-05-03 19:40:23 +0530

    day 1

--- day1.java
+++ day1.java
@@ -45,13 +45,13 @@ public class day1 {
         //     System.out.println(division + " is not divisible by either 3 or 5.");
         // }
         //vowels and consonants
-        char ch = sc.next().charAt(0);
-        if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' ||
-            ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U') {
-            System.out.println(ch + " is a vowel.");
-        } else {
-            System.out.println(ch + " is a consonant.");
-        }
+        // char ch = sc.next().charAt(0);
+        // if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' ||
+        //     ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U') {
+        //     System.out.println(ch + " is a vowel.");
+        // } else {
+        //     System.out.println(ch + " is a consonant.");
+        // }
         sc.close();
     }
 }
