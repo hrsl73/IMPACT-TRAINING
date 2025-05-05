b253104ab25c1816da064fa4627416a2cf07b6fd
Harshil <harshilmayani108@gmail.com>
2025-05-05 10:20:34 +0530

    day 3

---
+++ day3.java
@@ -0,0 +1,125 @@
+import java.util.Scanner;
+public class day3{
+    public static void main(String[] args){
+        //loops-for,while,do while,for each
+        Scanner sc = new Scanner(System.in);
+        // int n = sc.nextInt();
+        //prime number in for loop
+        // for (int i=2;i<=n;i++){
+        //     boolean isPrime = true;
+        //     for (int j=2;j*j<=i;j++){
+        //         if (i%j==0){
+        //             isPrime = false;
+        //             break;
+        //         }
+        //     }
+        //     if (isPrime){
+        //         System.out.println(i);
+        //     }
+        // }
+        
+
+        //to check prime number
+        // int num = sc.nextInt();
+
+        // for (int i=2;i*i<=num;i++){
+        //     if (num%i==0){
+        //         System.out.println("Not prime");
+        //         break;
+        //     }
+        //     else{
+        //         System.out.println("Prime");
+        //         break;
+        //     }
+        // }
+
+        // int n = sc.nextInt(); 
+        // String scr = Integer.toString(n); 
+        // System.out.println(scr.length());
+
+
+
+        // int count =0;
+        // for (int i=0;i<scr.length();i++){
+        //     count++;
+        // }
+        // System.out.println(count);
+
+        // for (int i=scr.length()-1;i>=0;i--){
+        //     System.out.print(scr.charAt(i));
+        // }
+        // while(n>0){
+        //     int reverse = n%10;
+        //     System.out.print(reverse);
+        //     n=n/10;
+        // }
+
+        // pattern based questions
+    
+        
+        // for (int i=1;i<=n;i++){
+        //     for (int j=1;j<=n-i;j++){
+        //         System.out.print(" ");
+        //     }
+        //     for (int j=1;j<=i;j++){
+        //         System.out.print("*");
+        //     }
+        //     System.out.println();
+        // }
+
+       
+
+        // for (int i=1;i<=n;i++){
+        //     for (int j=1;j<=n-i;j++){
+        //         System.out.print(" ");
+        //     }
+        //     for (int j=1;j<=i;j++){
+        //         System.out.print("*");
+        //     }
+        //     for (int j=1;j<i;j++){
+        //         System.out.print("*");
+        //     }
+        //     System.out.println();
+        // }
+
+
+        // for (int i=1;i<=n;i++){
+        //     for (int j=1;j<=i;j++){
+        //         System.out.print(j);
+        //     }
+        //     System.out.println();
+        // }
+
+        //jump statements
+
+        // for (int i=1;i<=10;i++){
+        //     if (i==4 || i==5){
+        //         continue;
+        //     }
+        //     System.out.println(i);
+        // }
+        // for(int i=1;i<=10;i++){
+        //     if (i==4){
+        //         break;
+        //     }
+        //     System.out.println(i);
+        // }
+        
+
+        // do while loop
+        //find hcf of two numbers
+        // int a = sc.nextInt();
+        // int b = sc.nextInt();
+        // int hcf = 1;
+        // int i = 1;
+        // do{
+        //     if (a%i==0 && b%i==0){
+        //         hcf =i;
+
+        //     }
+        //     i++;
+        // }
+        // while(i<=a && i<=b);
+        // System.out.println(hcf);
+    }
+}
