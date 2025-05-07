0c42dc5e06ea1820b1dc21da4c049dabe6c603e2
Harshil <harshilmayani108@gmail.com>
2025-05-07 15:16:19 +0530

    day 5

---
+++ day5.java
@@ -0,0 +1,130 @@
+public class day5{
+    public static void main(String[] args){
+        int arr[] = {6, 2, 2, 4, 5};
+
+        // System.out.print("array 1:");
+        // for(int i=0;i<arr.length;i++){
+        //     System.out.print(arr[i]);
+        // }
+
+
+
+        // int largest = arr[0];
+        // for (int i=0;i<arr.length;i++){
+        //     if (arr[i]>largest){
+        //         largest = arr[i];
+        //     }
+        // }
+        // System.out.println(largest);
+
+        // System.out.println(arr.length);
+        // for (int i=0;i<arr.length;i++){
+        //     System.out.print(arr[i]+5);
+        // }
+
+        // int smallest = arr[0];
+        // for (int i=0;i<arr.length;i++){
+        //     if (arr[i]<smallest){
+        //         smallest = arr[i];
+        //     }
+        // }
+        // System.out.println(smallest);
+
+
+
+        // int sum =0;
+        // for (int i=0;i<arr.length;i++){
+        //     sum+= arr[i];
+        // }
+        // System.out.println(sum);
+
+
+        // int sum =0;
+        // for (int i=0;i<arr.length;i++){
+        //     sum+= arr[i];
+        // }
+        // System.out.println(sum);
+        // System.out.println(sum/arr.length);
+
+
+        // int find = 3;
+        // for (int i=0;i<arr.length;i++){
+        //     if (arr[i]==find){
+        //         System.out.println("array found at index " +i);
+        //     }
+        // }
+
+        // int even =0;
+        // int odd =0;
+        // for (int i=0;i<arr.length;i++){
+        //     if (arr[i]%2==0){
+        //         even++;
+        //     }
+        //     else{
+        //         odd++;
+        //     }
+        // }
+        // System.out.println("even "+even+ " odd "+odd);
+
+        // for (int i=arr.length-1;i>=0;i--){
+        //     System.out.print(arr[i]+ " ");
+        // }
+
+
+
+        // int arr2[] = new int[arr.length];
+        // System.out.print("array 2:");
+        // for (int i=0;i<arr.length;i++){
+        //     arr2[i] = arr[i];
+        //     System.out.print(arr2[i]);
+        // }
+        
+        // for(int i=0;i<arr.length;i++){
+        //     for(int j=i+1;j<arr.length;j++){
+        //         if (arr[i]>arr[j]){
+        //             int temp = arr[i];
+        //             arr[i] = arr[j];
+        //             arr[j] = temp;
+        //         }
+        //     }
+        // }
+        // System.out.print("sorted array:");
+        // for(int i=0;i<arr.length;i++){
+        //     System.out.print(arr[i]);
+        // }
+        
+        // remove duplicates from array
+        // int arr2[] =new int[arr.length];
+        // int j=0;
+        // for (int i=0;i<arr.length;i++){
+        //     boolean found = false;
+        //     for (int k=0;k<j;k++){
+        //         if (arr[i]==arr2[k]){
+        //             found = true;
+        //             break;
+        //         }
+        //     }
+        //     if (!found){
+        //         arr2[j] = arr[i];
+        //         j++;
+        //     }
+        // }
+        // System.out.print("array without duplicates:");
+        // for (int i=0;i<j;i++){
+        //     System.out.print(arr2[i]);
+        // }
+        // int [][] numbers = {
+        //     {1, 2, 3},
+        //     {4, 5, 6},
+        //     {7, 8, 9}
+        // };
+
+        // for (int i=0;i<numbers.length;i++){
+        //     for (int j=0;j<numbers[i].length;j++){
+        //         System.out.print(numbers[i][j]+ " ");
+        //     }
+        //     System.out.println();
+        // }
+
+    }
+}
