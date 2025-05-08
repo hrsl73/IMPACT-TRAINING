af25053df4a1b771643cdc85dd01d7c1e9a36c8a
Harshil <harshilmayani108@gmail.com>
2025-05-08 11:26:29 +0530

    day 6

---
+++ day6.java
@@ -0,0 +1,40 @@
+import java.util.*;
+public class day6 {
+    public static  void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        int arr[] = {2,3,5,3,8,6};
+        int len = arr.length;
+        
+        // int arr2[] = {2,3,4,5,6,7,8,9,10};
+        // int arr2[] = new int[n];
+        // for(int i=0;i<n;i++){
+        //     arr2[i] = sc.nextInt();
+        // }
+        // int len = arr2.length;
+        // System.out.println(len);
+        // for(int i=0;i<len;i++){
+        //     System.out.print(arr2[i]+" ");
+        // }
+
+
+        // sort array(bubble sort)
+        // for (int i=0;i<len;i++){
+        //     int smallest = arr[i];
+        //     int x=i;
+        //     for (int j=i+1;j<len;j++){
+        //         if (arr[j]<smallest){
+        //             smallest = arr[j];
+        //             x=j;
+        //         }
+        //     }
+        //     int z = arr[i];
+        //     arr[i] = arr[x];
+        //     arr[x] =z ;
+        // }
+        // System.out.println("sorted array");
+        // for (int i=0;i<len;i++){
+        //     System.out.print(arr[i]+" ");
+        // }
+    }
+}
