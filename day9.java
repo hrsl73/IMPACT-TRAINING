200dd0622f7d95ca326ddcffc0aef5a26425da35
Harshil <harshilmayani108@gmail.com>
2025-05-12 11:18:09 +0530

    day 9

---
+++ day9.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+public class day9{
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+        //strings
+        // String s1 = sc.nextLine(); // more than one word
+        // String s2 = sc.next(); // single word
+        // System.out.println(s1);
+        // System.out.println(s2);
+        // int  n = s1.length();
+        // System.out.println(s1.length());
+
+        // for(int i=0;i<n;i++){
+        //     System.out.println(s1.charAt(i)); // prints each character of the string
+        // }
+        // String a = "helicoptorrrrrrhellicopterrheplicoptor";
+        // int count = 0;
+        // for(int i=0;i<a.length();i++){
+        //     char ch = a.charAt(i);
+        //     if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u'){
+        //         count++;
+        //     }
+        // }
+        // System.out.println("Number of vowels in the string: " + count);
+
+        // System.out.println(s1.equals(s2)); // checks if two strings are equal
+        // String y = new String("hello");
+
+        // String x = "hello worrld";
+        // String y = x.substring(0,5);
+        // System.out.println(y);
+    }
+}
